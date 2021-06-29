2f0c326380edfc43cc2037687fe46bef34540627
guilleorama <guilleorama@gmail.com>
2021-06-28 23:02:39 -0300

    commit de inicio => bugardo

---
+++ num/lonumero.py
@@ -0,0 +1,18 @@
+from flask import Flask, session, g, render_template
+
+
+
+
+
+
+
+
+# numeros = 3591
+# tirada = input("ingrese los 4 digitos\n")
+
+# if numeroOK(tirada) == True:
+#     print(valorarTirada(numeros, tirada))
+# else:
+#     print(numeroOK(tirada))
+
+
