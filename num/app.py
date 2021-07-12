93b057ca79b8434b1f98c9e8f3bdb8286dd0189d
guilleorama <guilleorama@gmail.com>
2021-07-11 23:44:05 -0300

    agregado de mail en coockie

--- num/app.py
+++ num/app.py
@@ -9,19 +9,21 @@ host = get_env_config('DBHOST', 'env.cfg')
 secretkey = get_env_config('SECRETKEY', 'env.cfg')
 dbuser = get_env_config('DBUSER', 'env.cfg')
 password = get_env_config('DBPASS', 'env.cfg')
-
+debugOn = get_env_config('DEBUG', 'env.cfg')
+print(f'db:{db}, user:{dbuser}, pass:{password}, host:{host}, key:{secretkey}, debug:{debugOn}')
 mysql = MySQL()
+mysocket = SocketIO()
 
 
-def create_app(debug=False):
+def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mi_gran_clave_secreta'
-    app.debug = debug
+    app.debug = debugOn
+    app.config['SECRET_KEY'] = secretkey
 
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'guille'
-    app.config['MYSQL_PASSWORD'] = 'mypass'
-    app.config['MYSQL_DB'] = 'mycrud'
+    app.config['MYSQL_HOST'] = host
+    app.config['MYSQL_USER'] = dbuser
+    app.config['MYSQL_PASSWORD'] = password
+    app.config['MYSQL_DB'] = db
     mysql = MySQL(app)
 
     # blueprint
@@ -32,12 +34,12 @@ def create_app(debug=False):
     from no_auth_main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # mysocket.init_app(app)
+    mysocket.init_app(app, logger=True, engineio_logger=True)
 
     return app
 
 
-if __name__ == '__main__':
-    app = create_app(debug=True)
-    mysocket = SocketIO(app, logger=True, engineio_logger=True)
-    mysocket.run(app, port=5000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app = create_app()
+#     mysocket = SocketIO(app, logger=True, engineio_logger=True)
+#     mysocket.run(app, port=5000, host='0.0.0.0')
